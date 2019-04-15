da288c9003c1beb9ade2181b67d83409be046595
Célia Casagrande <casagrande.celia@gmail.com>
2019-04-15 19:19:38 +0200

    create home fragment and change dark primary color

--- app/src/main/java/com/esgi/project/underdico/MainActivity.java
+++ app/src/main/java/com/esgi/project/underdico/MainActivity.java
@@ -77,7 +77,7 @@ public class MainActivity extends AppCompatActivity
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
+
         int id = item.getItemId();
 
         if (id == R.id.menu_home) {
