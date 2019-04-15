750edb22de7b90e691afb7abe807503d31c878e3
Célia Casagrande <casagrande.celia@gmail.com>
2019-04-15 19:07:45 +0200

    design navigation drawer

--- app/src/main/java/com/esgi/project/underdico/MainActivity.java
+++ app/src/main/java/com/esgi/project/underdico/MainActivity.java
@@ -80,17 +80,13 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.menu_home) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.menu_top) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.menu_new) {
 
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.menu_play) {
 
         }
 
