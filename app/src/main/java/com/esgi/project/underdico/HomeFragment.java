3fa15aebcfbf4f563564079d55ef4e3c95771863
Célia Casagrande <casagrande.celia@gmail.com>
2019-04-18 20:24:16 +0200

    design login activity

--- app/src/main/java/com/esgi/project/underdico/HomeFragment.java
+++ app/src/main/java/com/esgi/project/underdico/HomeFragment.java
@@ -3,10 +3,13 @@ package com.esgi.project.underdico;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.constraint.ConstraintLayout;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 
 /**
@@ -16,6 +19,8 @@ import android.view.ViewGroup;
  */
 public class HomeFragment extends Fragment {
 
+    ImageButton closeDayExpression;
+    ConstraintLayout dayExpressionLayout;
     public HomeFragment() {
         // Required empty public constructor
     }
@@ -32,13 +37,36 @@ public class HomeFragment extends Fragment {
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        assignViews();
+        setOnClickListeners();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+
+
+    public void assignViews() {
+        closeDayExpression = getView().findViewById(R.id.closeDayExpressionButton);
+        dayExpressionLayout = getView().findViewById(R.id.dayExpressionLayout);
+    }
+
+    public void setOnClickListeners() {
+        closeDayExpression.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                hideDayExpression();
+            }
+        });
+    }
+
+    public void hideDayExpression() {
+        dayExpressionLayout.setVisibility(View.GONE);
     }
 }
