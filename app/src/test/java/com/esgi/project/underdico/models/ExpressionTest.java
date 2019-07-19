ff36e9b4d7d38d70df07bc4bf0e95cf16af20e36
csgrdcelia <casagrande.celia@gmail.com>
2019-07-19 08:18:23 +0200

    Fix tests

--- app/src/test/java/com/esgi/project/underdico/models/ExpressionTest.java
+++ app/src/test/java/com/esgi/project/underdico/models/ExpressionTest.java
@@ -9,19 +9,19 @@ public class ExpressionTest {
 
     @Test
     public void shouldSimpleWord_resultInHiddenWord() {
-        Expression expression = new Expression("Afrotrap", null, null, null);
+        Expression expression = new Expression("Afrotrap", null, null, null,null);
         Assert.assertEquals("_ _ _ _ _ _ _ _", expression.getHiddenWord());
     }
 
     @Test
     public void shouldWordWithSpecialChar_resultInHiddenWord() {
-        Expression expression = new Expression("Afro-trap", null, null, null);
+        Expression expression = new Expression("Afro-trap", null, null, null, null);
         Assert.assertEquals("_ _ _ _ - _ _ _ _", expression.getHiddenWord());
     }
 
     @Test
     public void shouldWordWithSpace_resultInHiddenWord() {
-        Expression expression = new Expression("Afro trap", null, null, null);
+        Expression expression = new Expression("Afro trap", null, null,null, null);
         Assert.assertEquals("_ _ _ _   _ _ _ _", expression.getHiddenWord());
     }
 }
